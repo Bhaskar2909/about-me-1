c0487b04795a88eebfb56bc1a839c8b8cb858efe
harish24680 <S538362@nwmissouri.edu>
2020-03-06 23:01:23 -0600

    Commit

--- codes/code.js
+++ codes/code.js
@@ -2,11 +2,13 @@
 const  name = document.querySelector("#name");
 const  email = document.querySelector("#email");
 const  msg = document.querySelector("#msg");
+const  contact = document.querySelector("#contact");
 formbtn.addEventListener("click", store);
 
 function store(){
     localStorage.setItem("Name",name.value)
     localStorage.setItem("Email",email.value)
     localStorage.setItem("Message",msg.value)
-    console.log(672878);
+    // console.log(672878);
+    contact.innerHTML = `Hi, ${localStorage.getItem("Name")}. Thank for you Message. I'll contact you shortly by your Email ${localStorage.getItem("Email")}`;
 }
